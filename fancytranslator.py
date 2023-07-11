f99f836b49d24ef938f213c181791e46a3d00c0e
mruchus <mruchus@icloud.com>
2023-07-11 15:22:52 +0100

    add file

--- fancytranslator.py
+++ fancytranslator.py
@@ -9,13 +9,15 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/mruchus/translate.json"
 def rephrase_text(text):
     response = openai.Completion.create(
         engine="text-davinci-003",  # model
-        prompt=f"{text}\n\n Rewrite the sentence in the style of a news article:",
+        prompt=f"{text}\n\n Rewrite this in the style of a formal presentation. Keep the length about the same as the original.",
         temperature=0.5, # randomness (lower == more deterministic)
-        max_tokens=120 # 100 tokens ~= 75 words
+        max_tokens=1000 # 100 tokens ~= 75 words
     )
 
     # note: 'response.choices' contains model outputs, typically one unless specified otherwise
-    return response.choices[0].text.strip()
+    reply = response.choices[0].text.strip()
+    print(reply)
+    return reply
 
 def translate_text(text, target='en'):
     translate_client = translate.Client()
@@ -37,7 +39,11 @@ def to_fancy(text):
     return fancy_english
 
 def to_gradio():
-    demo = gr.Interface(fn=to_fancy, inputs="text", outputs="text")
+    demo = gr.Interface(fn=to_fancy,
+                        inputs="text",
+                        outputs="text",
+                        title="Fancy To-English Translator",
+                        description="Translate foreign text into different styles of English!")
     demo.launch(share=True)
 
 to_gradio()
